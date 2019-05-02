054e007e10e24e69f419ea867091288abec124e4
willcl-ark <will8clark@gmail.com>
2019-05-02 12:57:29 +0100

    Add helper conversions for binary, base64 and SH256 hash.
    Add 32 byte preimage generator using secrets module

---
+++ utilities.py
@@ -0,0 +1,31 @@
+from secrets import token_bytes
+from hashlib import sha256
+from binascii import hexlify, unhexlify, a2b_base64, b2a_base64
+
+
+def gen_32_byte_preimage():
+    return token_bytes(32)
+
+
+def bytes_to_hex(byte_string):
+    return hexlify(byte_string)
+
+
+def hex_to_bytes(hex_string):
+    return unhexlify(hex_string)
+
+
+def bytes_to_base64(byte_string):
+    return b2a_base64(byte_string)
+
+
+def base64_to_bytes(base64_string):
+    return a2b_base64(base64_string)
+
+
+def sha256_to_bytes(byte_string):
+    return sha256(byte_string).digest()
+
+
+def sha256_to_hex(byte_string):
+    return sha256(byte_string).hexdigest()
